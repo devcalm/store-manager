dd62496bebb6806eba68d5a4393d8d736f00cbf5
Oleksii Tsvietaiev <o.tsvietaiev92@gmail.com>
2024-11-22 11:32:29 +0200

    test(vendor): cover fetcher Vendor

--- src/test/java/org/devcalm/store/manager/data/VendorTestData.java
+++ src/test/java/org/devcalm/store/manager/data/VendorTestData.java
@@ -6,6 +6,7 @@ import org.instancio.Instancio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.instancio.Select.field;
 
 @Component
@@ -13,6 +14,12 @@ public class VendorTestData {
 
     @Autowired
     private VendorRepository vendorRepository;
+    @Autowired
+    private CategoryTestData categoryTestData;
+    @Autowired
+    private ProductTestData productTestData;
+    @Autowired
+    private StoreTestData storeTestData;
 
     public Vendor createVendor() {
         return vendorRepository.save(Instancio.of(Vendor.class)
@@ -22,4 +29,13 @@ public class VendorTestData {
                 .ignore(field(Vendor::getStoreIds))
                 .create()).block();
     }
+
+    public Vendor createVendorWithRelations() {
+        var vendor = createVendor();
+        assertThat(categoryTestData.createCategory(vendor.getId())).isNotNull();
+        assertThat(productTestData.createProduct(vendor.getId())).isNotNull();
+        assertThat(storeTestData.createStore(vendor.getId())).isNotNull();
+        return vendor;
+    }
+
 }
--- src/test/java/org/devcalm/store/manager/service/vendor/VendorFetcherTest.java
+++ src/test/java/org/devcalm/store/manager/service/vendor/VendorFetcherTest.java
@@ -4,14 +4,21 @@ import org.bson.types.ObjectId;
 import org.devcalm.store.manager.MongoTestConfig;
 import org.devcalm.store.manager.data.VendorTestData;
 import org.devcalm.store.manager.domain.exception.EntityNotFoundException;
+import org.devcalm.store.manager.domain.model.Vendor;
+import org.devcalm.store.manager.domain.repository.VendorRepository;
 import org.devcalm.store.manager.infrastructure.reflection.FieldNamesExtractor;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ContextConfiguration;
 import reactor.test.StepVerifier;
+
+import java.util.function.Predicate;
 
 @DataMongoTest
 @ContextConfiguration(classes = MongoTestConfig.class)
@@ -24,6 +31,13 @@ class VendorFetcherTest {
     private VendorTestData testData;
     @Autowired
     private VendorFetcher fetcher;
+    @Autowired
+    private VendorRepository vendorRepository;
+
+    @BeforeEach
+    void setUp() {
+        vendorRepository.deleteAll().block();
+    }
 
     @Test
     void shouldFindById() {
@@ -42,5 +56,44 @@ class VendorFetcherTest {
                 .expectErrorMatches(throwable -> throwable instanceof EntityNotFoundException &&
                         throwable.getMessage().equals("Vendor %s is not found.".formatted(id)))
                 .verify();
+    }
+
+    @Test
+    void shouldFindByIdWithRelations() {
+        var vendor = testData.createVendorWithRelations();
+
+        Predicate<Vendor> condition = getVendorPredicate(vendor);
+
+        StepVerifier.create(fetcher.findByIdWithRelations(vendor.getId()))
+                .expectNextMatches(condition)
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldThrowExceptionWhenVendorNotFoundWithRelations() {
+        var id = new ObjectId();
+
+        StepVerifier.create(fetcher.findByIdWithRelations(id))
+                .expectErrorMatches(throwable -> throwable instanceof EntityNotFoundException &&
+                        throwable.getMessage().equals("Vendor %s is not found.".formatted(id)))
+                .verify();
+    }
+
+    @Test
+    void shouldFindAllWithRelations() {
+        var vendor = testData.createVendorWithRelations();
+
+        Predicate<Vendor> condition = getVendorPredicate(vendor);
+
+        StepVerifier.create(fetcher.findAllWithRelations(Pageable.ofSize(1)))
+                .expectNextMatches(condition)
+                .verifyComplete();
+    }
+
+    private static @NotNull Predicate<Vendor> getVendorPredicate(Vendor vendor) {
+        return v -> v.getId().equals(vendor.getId())
+                && !v.getStoreIds().isEmpty()
+                && !v.getCategoryIds().isEmpty()
+                && !v.getProductIds().isEmpty();
     }
 }
